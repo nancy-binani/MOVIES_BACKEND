87d5c8598f490de406e6ef7b1f3486990f20e402
Nancy Binani <nancybinani1207@gmail.com>
2021-07-16 16:43:42 +0530

    up

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 let express = require("express")
+let data = require("./data.json")
+
 let server = express() //nya server banadegi but chalu nhi karti h
 
 //ye line server ko shuru kardeti h ek port pr
 
 server.get("/movies",function(req,res){
-    res.send("movies data from server")
+    res.send(data)
 })
 
 server.get("/genre",function(req,res){
-    res.send("genre data from server")
-})
+   let allGenreObjects = data.map(function(e){
+       return e.genre;
+    })
+       let uniqueGenreObjects = []
 
-server.listen(3000)
+       for(let i=0;i<allGenreObjects.length;i++){
+           let genreId = allGenreObjects[i]["_id"]
+           let index = uniqueGenreObjects.findIndex(function(e){
+               return e._id == genreId
+           })
+
+           if(index==-1){
+               uniqueGenreObjects.push(allGenreObjects[i]);
+           }
+       }
+       res.json(uniqueGenreObjects)
+   })
+
+
+server.listen(4000)
